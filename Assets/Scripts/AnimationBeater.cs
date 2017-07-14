3417b41a2362e936e2827028bc0c4034d65a7d99
Stefan Zaufl <stefan.zaufl@hotmail.com>
2017-07-14 20:53:25 +0200

    Added a dancing cube

---
+++ Assets/Scripts/AnimationBeater.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationBeater : MonoBehaviour
+{
+
+	private static readonly int BEAT = Animator.StringToHash("Beat");
+	private BeatMaster beatMaster;
+	private Animator animator;
+
+	// Use this for initialization
+	void OnEnable()
+	{
+		GameObject bmGO = GameObject.FindWithTag("Music");
+		beatMaster = bmGO.GetComponent<BeatMaster>();
+		animator = GetComponent<Animator>();
+
+		beatMaster.beatEvent += BeatMasterOnBeatEvent;
+	}
+
+	void OnDisable()
+	{
+		if (beatMaster != null)
+			beatMaster.beatEvent -= BeatMasterOnBeatEvent;
+	}
+
+	private void BeatMasterOnBeatEvent()
+	{
+		animator.SetTrigger(BEAT);
+	}
+}
