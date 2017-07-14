07fa6e2081a33a1f2bb2738fb07beec6cdea51b7
Stefan Zaufl <stefan.zaufl@hotmail.com>
2017-07-14 20:21:19 +0200

    Init Commit

---
+++ Assets/Scripts/BeatLight.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatLight : MonoBehaviour
+{
+	public Light lightSource;
+	public Color[] colors;
+	private BeatMaster beatMaster;
+	private int colorIndex = 0;
+
+	// Use this for initialization
+	void OnEnable ()
+	{
+		GameObject bmGO = GameObject.FindWithTag("Music");
+		beatMaster = bmGO.GetComponent<BeatMaster>();
+
+		beatMaster.beatEvent += BeatMasterOnBeatEvent;
+	}
+
+	void OnDisable()
+	{
+		if(beatMaster != null)
+			beatMaster.beatEvent -= BeatMasterOnBeatEvent;
+	}
+
+	private void BeatMasterOnBeatEvent()
+	{
+		lightSource.color = colors[colorIndex];
+
+		if (++colorIndex >= colors.Length)
+		{
+			colorIndex = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/BeatMaster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatMaster : MonoBehaviour
+{
+
+	public delegate void BeatDelegate();
+
+	public event BeatDelegate beatEvent;
+
+	public AudioSource musicSource;
+	public float beatOffset;
+	public float bps;
+
+	private int lastBeatIndex = -1;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		int beat = getBeatIndex(musicSource.time - beatOffset);
+
+		if (beat > lastBeatIndex)
+		{
+			lastBeatIndex = beat;
+			if (beatEvent != null)
+			{
+				beatEvent();
+			}
+		}
+	}
+
+	private int getBeatIndex(float time)
+	{
+		return Mathf.FloorToInt(time * bps);
+	}
+}
