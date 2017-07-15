292061e8c6ec4da3137b20fcb02b13050dfd265f
Stefan Zaufl <stefan.zaufl@hotmail.com>
2017-07-15 15:12:36 +0200

    Added more music and a system to easily change between them

--- Assets/Scripts/BeatMaster.cs
+++ Assets/Scripts/BeatMaster.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class BeatMaster : MonoBehaviour
 {
 
@@ -9,13 +11,19 @@ public class BeatMaster : MonoBehaviour
 
 	public event BeatDelegate beatEvent;
 
-	public AudioSource musicSource;
-	public float beatOffset;
-	public float bps;
+	private AudioSource musicSource;
+	public SongInfo songInfo;
 
     public float maxBeatDiff = 0.1f;
 
 	private int lastBeatIndex = -1;
+
+	void Awake()
+	{
+		musicSource = GetComponent<AudioSource>();
+		musicSource.clip = songInfo.song;
+		musicSource.Play();
+	}
 	
 	// Update is called once per frame
 	void Update ()
@@ -34,11 +42,11 @@ public class BeatMaster : MonoBehaviour
 
     public bool allowsJump()
     {
-        float fCurrentTimeBeat = getCurrentTime() * bps;
+        float fCurrentTimeBeat = getCurrentTime() * songInfo.Bps;
         float fNearestBeat = Mathf.Floor(fCurrentTimeBeat + 0.5f);
 
         float fDiff = Mathf.Abs(fCurrentTimeBeat - fNearestBeat);
-        fDiff /= bps;
+        fDiff /= songInfo.Bps;
 
 		//Debug.Log(string.Format("Beat diff: {0}", fDiff));
 
@@ -48,17 +56,17 @@ public class BeatMaster : MonoBehaviour
 
 	private int getBeatIndex(float time)
 	{
-		return Mathf.FloorToInt(time * bps);
+		return Mathf.FloorToInt(time * songInfo.Bps);
 	}
 
     private float getCurrentTime()
     {
-        return musicSource.time - beatOffset;
+        return musicSource.time - songInfo.offset;
     }
 
 	public int NearestBeat
 	{
-		get { return Mathf.RoundToInt(getCurrentTime() * bps); }
+		get { return Mathf.RoundToInt(getCurrentTime() * songInfo.Bps); }
 	}
 
 	public int LastBeat { get { return lastBeatIndex; } }
---
+++ Assets/Scripts/SongInfo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	[CreateAssetMenu(fileName = "SongInfo", menuName = "Song Information", order = 0)]
+	public class SongInfo : ScriptableObject
+	{
+		public AudioClip song;
+		public int bpm;
+		public float offset;
+
+		public float Bps { get { return bpm / 60f; } }
+	}
+}
