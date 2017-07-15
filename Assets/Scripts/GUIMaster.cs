7be0ef62c9205eb26c2295d7a89d6cde6d02a419
Stefan Zaufl <stefan.zaufl@hotmail.com>
2017-07-15 17:39:22 +0200

    Refactored GUI to the modern GUI, created a new prefab for the canvas

--- Assets/Scripts/GUIMaster.cs
+++ Assets/Scripts/GUIMaster.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GUIMaster : MonoBehaviour {
 
-    private GUIText m_tOverlayLine1;
-    private GUIText m_tOverlayLine2;
+    private Text m_tOverlayLine1;
+    private Text m_tOverlayLine2;
 
 	// Use this for initialization
 	void Start () {
         GameObject line1GO = GameObject.FindGameObjectWithTag("TextLine1");
         if (line1GO != null)
         {
-            m_tOverlayLine1 = line1GO.GetComponent<GUIText>();
+            m_tOverlayLine1 = line1GO.GetComponent<Text>();
             if (m_tOverlayLine1 != null)
                 m_tOverlayLine1.enabled = false;
         }
@@ -20,7 +21,7 @@ public class GUIMaster : MonoBehaviour {
         GameObject line2GO = GameObject.FindGameObjectWithTag("TextLine2");
         if (line2GO != null)
         {
-            m_tOverlayLine2 = line2GO.GetComponent<GUIText>();
+            m_tOverlayLine2 = line2GO.GetComponent<Text>();
             if (m_tOverlayLine2 != null)
                 m_tOverlayLine2.enabled = false;
         }
